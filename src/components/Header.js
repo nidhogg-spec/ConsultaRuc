3a1d0a08b1cac17cd5a56e0439007bea55291a66
Camilo Reynaga <camilo.reynaga@gmail.com>
2019-09-17 12:56:47 -0500

    mejora header y footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './styles/estilosheader.css'
+import imgsunat from './img/imgsunat.png'
 
 function Header() {
     return(
-        <header className="header"></header>
+        <header className="header">
+            <div className="flex-header">
+                <div><h1>Consulta con tu numero de RUC</h1></div>
+                <div><img src={imgsunat}></img></div>
+                <div><h3>"Rapido, Ligero y Seguro"</h3></div>
+            </div>
+        </header>
     );
 }
 
