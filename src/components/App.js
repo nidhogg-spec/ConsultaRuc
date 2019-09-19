ad9ee06762bc229b2ca34e58735a18fb9d46faf0
oldair <lsaire97@gmail.com>
2019-09-19 09:52:05 -0500

    Router totalemente funcional

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import ConsultaRuc from '../pages/ConsultaRuc';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/ConsultaRuc" component={ConsultaRuc} />
         </Switch>
     </BrowserRouter>
   );
--- src/pages/ConsultaRuc.js
+++ src/pages/ConsultaRuc.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import FormConsulta from '../components/FormConsulta';
 import Header from '../components/Header';
 import Footer from "../components/Footer";
-import { Link } from 'react-router-dom';
 
-function App() {
+class ConsultaRuc extends React.Component {
+  render() {
   return (
     <div className="App container">
       <Header></Header>
+      <FormConsulta></FormConsulta>
       <Footer></Footer>
     </div>
   );
+  }
 }
 
-export default App;
+export default ConsultaRuc;
